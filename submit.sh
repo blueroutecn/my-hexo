d41ca2ee15916aa4bf5d26f2064969f3534599cb
HouJP <houjp1992@gmail.com>
2016-06-04 16:20:56 +0800

    add personal info

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: submit.sh
+# Author: HouJP
+# mail: houjp1992@gmail.com
+# Created Time: 三  9/16 21:58:46 2015
+#########################################################################
+# !/bin/bash
+
+git add --all .
+git commit -m "$1"
+git push -u origin master
